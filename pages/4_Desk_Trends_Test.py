f047bc2be69c2f0074b3b948e109281bb30971c4
Matheus R Dias <matheus.regis@me.com>
2023-10-03 14:50:56 +0000

    Changing the requirement for the matplotlib

--- pages/4_Desk_Trends_Test.py
+++ pages/4_Desk_Trends_Test.py
@@ -1,8 +1,7 @@
 # Import Streamlit and Pandas
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-
+import altair as alt
 
 # Dummy Data for different plots
 # Replace these with your actual data
@@ -31,29 +30,19 @@ data_upsell = pd.DataFrame({
     'count': [30, 170]
 })
 
+
 # Title
 st.title("Desk Trends")
 
+# Create two columns for Plot 1 and Plot 2
+col1, col2 = st.columns(2)
+
 # Plot 1: Most common reasons for customers to get in touch
-st.subheader("Most Common Reasons for Customer Contact")
-st.bar_chart(data_reason.set_index('reason')['count'])
+with col1:
+    st.subheader("Most Common Reasons for Customer Contact")
+    st.bar_chart(data_reason.set_index('reason')['count'])
 
 # Plot 2: Most common demands from customers
-st.subheader("Most Common Demands from Customers")
-st.bar_chart(data_demand.set_index('demand')['count'])
-
-# Plot 3: Most common actions done by helpdesk
-st.subheader("Most Common Actions by Helpdesk")
-st.bar_chart(data_action.set_index('action')['count'])
-
-# Plot 4: Pie chart for sentiment
-st.subheader("Customer Sentiment")
-fig, ax = plt.subplots()
-ax.pie(data_sentiment['count'], labels=data_sentiment['sentiment'], autopct='%1.1f%%')
-st.pyplot(fig)
-
-# Plot 5: Pie chart for upsell or cross-sell opportunity
-st.subheader("Upsell or Cross-sell Opportunities")
-fig, ax = plt.subplots()
-ax.pie(data_upsell['count'], labels=data_upsell['upsell_opportunity'], autopct='%1.1f%%')
-st.pyplot(fig)
+with col2:
+    st.subheader("Most Common Demands from Customers")
+    st.bar_chart(data_demand.set_index('demand')['count'])
